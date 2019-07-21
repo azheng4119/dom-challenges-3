b468fec9ce6c88178bfd5d4532c5ce782633b4e0
azheng4119 <azheng4119@bths.edu>
2019-07-21 11:31:20 -0400

    fill all and fill uncolored

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
 let addRow = () => {
     let tempTr = document.createElement('tr');
     let listOfTr = document.getElementsByTagName('tr');
-    if (listOfTr.length === 0){
+    let amountOfTr = listOfTr.length;
+    if (amountOfTr === 0){
+        amountOfTr = 1;
+    }else{
+        amountOfTr = listOfTr[0].getElementsByTagName('td').length;
+    }
+    for (let i = 0; i < amountOfTr; i++){
         let tempTd = document.createElement('td');
         tempTd.setAttribute("id", "cell");
         tempTd.width = "50px";
         tempTd.height = "50px";
         tempTd.addEventListener("mouseover", function() {
-            if(this.style.backgroundColor == "blue") {
+            if(this.style.backgroundColor != "whitesmoke") {
                 this.style.backgroundColor = "whitesmoke";
             }
             else {
-                this.style.backgroundColor = "blue";
+                this.style.backgroundColor = document.getElementById('colordrop').value;
             }
         });
-        
-        tempTr.append(tempTd);
-        
-    }else{
-        for (let i = 0; i < listOfTr[0].getElementsByTagName('td').length; i++){
-            let tempTd = document.createElement('td');
-            tempTd.setAttribute("id", "cell");
-            tempTd.width = "50px";
-            tempTd.height = "50px";
-            tempTd.addEventListener("mouseover", function() {
-                if(this.style.backgroundColor == "blue") {
-                    this.style.backgroundColor = "whitesmoke";
-                }
-                else {
-                    this.style.backgroundColor = "blue";
-                }
-            });
-            tempTr.append(tempTd);   
-        }
+        tempTr.append(tempTd);   
     }
     document.getElementById('main').append(tempTr);
   
@@ -53,11 +41,11 @@ let addColumn = () =>{
             tempTd.width = "50px";
             tempTd.height = "50px";
             tempTd.addEventListener("mouseover", function() {
-                if(this.style.backgroundColor == "blue") {
+                if(this.style.backgroundColor != "whitesmoke") {
                     this.style.backgroundColor = "whitesmoke";
                 }
                 else {
-                    this.style.backgroundColor = "blue";
+                    this.style.backgroundColor = document.getElementById('colordrop').value;
                 }
             });
             listOfTr[i].append(tempTd);
@@ -72,9 +60,19 @@ let removeColumn = () => {
         listOfTr[i].lastChild.remove();
     }
 }
-let fillAll = () => {
+let fillAll = (color) => {
     let listOfTr = document.getElementsByTagName('td');
     for ( let i = 0; i < listOfTr.length; i++){
-        listOfTr[i].style.backgroundColor = "blue";
+        listOfTr[i].style.backgroundColor = color;
+    }
+}
+
+let fillUnColored = () => {
+    let listOfTr = document.getElementsByTagName('td');
+    for ( let i = 0; i < listOfTr.length; i++){
+        if (listOfTr[i].style.backgroundColor == 'whitesmoke'){
+            listOfTr[i].style.backgroundColor = document.getElementById('colordrop').value;
+            //console.log(listOfTr[i].style.backgroundColor);
+        }
     }
 }
